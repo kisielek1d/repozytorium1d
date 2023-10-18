f09538285529cb92d9932d131634957f033f13cb
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-18 07:45:28 +0200

    lkmkm

--- 2/2/2.cpp
+++ 2/2/2.cpp
@@ -1,21 +1,14 @@
-// 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";  
-    std::cout << "Mam na Imie Filip\n";
-    std::cout << "Mam na Imie Filip\n";
-    std::cout << 'X'; //sta³a znakowa-musi byæ jeden znak
-    std::cout << "\n";
-    std::cout << "\n";
-    std::cout << 57574 << "\n"; //sta³a liczbowa ca³kowita
-    std::cout << 12.5 << "\n"; //sta³a liczbowa rzeczywista
+    std::cout << "Hello World!\n";
 }
 
-// run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
