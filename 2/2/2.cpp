7d437a9fd26395753949905b1283eec7660fdb5a
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-25 09:35:00 +0200

    okokok

--- 2/2/2.cpp
+++ 2/2/2.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    
+    short ageFromUser; 
 }
 
 /*
@@ -14,7 +14,7 @@ Sposób zapisu:
 * w punktach
 * opis s³owny
 * jêzyk programowania lub pseudokod
-* zbiór rysunków
+* zbiór rysunkówa
 * schemat blokowy
  
  Zmienna - jest to pewien obszar pamiêci s³u¿¹cy do przechowywania danych. Te dane mo¿na modyfikowaæ
@@ -23,9 +23,35 @@ Sposób zapisu:
  Daklaracja zmiennej - moment jej utworzenia.
 
  typ_zmiennej nazwa_zmiennej;
-
+ w
  Typ zmiennej -  mówi o wielkoœci obszaru w pamiêci i rodzaju przechowywanych danych.
-short   liczby ca³kowite ze znakiem    2 bajty  <-32 768; 32 727>
-int     liczby ca³kowite ze znakiem    4 bajty  <-2 147 483 648; 2 147 483 647>
+short           liczby ca³kowite ze znakiem     2 bajty  <-32 768; 32 727>
+int/long        liczby ca³kowite ze znakiem     4 bajty  <-2 147 483 648; 2 147 483 647>
+long long       liczby ca³kowite ze znakiem     8 bajtów <-9 223 372 036 854 775 808; 9 223 372 036 854 775 807>  
 
+Je¿eli przed powy¿szymi typami dodamy s³owo "unsigned" to zakres jest od zera do podwojonego zakresu górnego plus 1.
+
+float           liczby rzeczywiste ze znakiem   4 bajty
+double          liczby rzeczywiste ze znakiem   8 bajtów
+long double     liczby rzeczywiste ze znakiem   12 bajtów
+
+Nazwa zmiennej - nazwa oszaru w pamiêci. Poprzez te nazwe odwo³ujemy sie do tego obszaru.
+
+Wymagania kompilatora wzglêdem nazwy:
+*u¿ycie tylko dozwolonych znaków 
+- alfabet angielski od a-z oraz A - Z
+-cyfry arabskie 0 - 9                                                           * *
+-podkreœlenie (pod³oga)                                                         ---
+*pierwszym znakiem nie mo¿e byæ cyfra
+* nie mo¿e to byæ s³owo kluczowe danego jêzyka programowania
+* musi byc unikalna w danej woidocznoœci
+
+Wymagania programistów:
+* jeœli nazwa sk³ada soê z wielu wyrazów to zamiast spacji:
+    -u¿ywamy znaków podkreœlenia np. first_numer_from_user
+    -stosujemy camel case, czyli piszemy wszystko razem a s³owa (zaczynamy od drugiego)
+    zaczynamy z du¿ej litery np. firstNumberForUser
+* nazwa zmiennej musi oddawaæ charakter przechowywanych danych, czyli miec znaczenie
+* nazwy po angielsku!!!!!!!
+    
 */
---
+++ 2/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,13 @@
+// VariableConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    short ageFromUser;
+    std::cout << "Podaj swoj wiek, \n";
+    std::cin >> ageFromUser;
+
+    std::cout << "Masz " << ageFromUser << " lat.\n";
+}
