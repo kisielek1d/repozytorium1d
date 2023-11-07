839614644bb0a7f7e4827a0e4867826ae5324032
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-07 10:10:48 +0100

    vbvbvbvb

--- 2/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ 2/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -2,12 +2,37 @@
 //
 
 #include <iostream>
-
-int main()
+void task1()
 {
     short ageFromUser;
     std::cout << "Podaj swoj wiek, \n";
     std::cin >> ageFromUser;
 
     std::cout << "Masz " << ageFromUser << " lat.\n";
+}
+
+//Stwórz program, który pobiera od u¿ytkownika
+//d³ugoœæ w metrach i przelicza j¹ na centymetry
+//milimetry i kilometry. Wyœwietl wynik.
+void task2()
+{
+    float LengthInMeterFromUser;
+    std::cout << "Podaj d³ugoœæ w metrach \n";
+    std::cin >> LengthInMeterFromUser;
+
+    float LengthInCentimeter;
+        LengthInCentimeter = LengthInMeterFromUser / 100;
+
+        float LenghtInMilimeter;
+        LenghtInMilimeter = LengthInCentimeter / 10;
+
+    float LenghtInKilometer;
+        LenghtInKilometer = LengthInMeterFromUser * 1000;
+
+}
+
+int main()
+{
+    //task1();
+    task2();
 }
