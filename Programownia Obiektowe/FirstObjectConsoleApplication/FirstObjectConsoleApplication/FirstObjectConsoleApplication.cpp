b00164a96639e635ee200424678632d0b695d8da
q <q>
2024-10-09 09:00:15 +0200

    gfgh

--- Programownia Obiektowe/FirstObjectConsoleApplication/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programownia Obiektowe/FirstObjectConsoleApplication/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -1,47 +1,22 @@
-﻿
-#include<iostream>
-
+﻿#include <iostream>
 
 class BankAccount
 {
 public:
 	double balance; // saldo
-	std::string owner; //właciciel
+	std::string owner; //w³aœciciel
 	std::string currency; //waluta
 
+	void AccountInformation();
 
+	void DepositToAccount(double amount);
 
-	void AccountInformation()
-	{
-		std::cout << "Informacja o koncie bankowym.\n";
-		std::cout << "W³aœciciel: " << owner << "\n";
-		std::cout << "Saldo: " << balance << " " << currency << "\n";
-	}
+	bool WidthdrawalFromAccount(double amount);
+
+	void TransferBetweenAcounts(BankAccount& targetAccount, double amount);
 };
-void depositToAccount(BankAccount& account, double amount)
-{
-	amount = abs(amount);
-	account.balance = account.balance + amount;
-}
 
-bool widthdrawalFromAccount(BankAccount& account, double amount)
-{
-	amount = abs(amount);
-	if (account.balance - amount >= 0)
-	{
-		account.balance = account.balance - amount;
-		return true;
-	}
-	return false;
-}
-
-void transferBetweenAcounts(BankAccount& sourceAccount, BankAccount& targetAccount, double amount)
-{
-	if (widthdrawalFromAccount(sourceAccount, amount) == true)
-		depositToAccount(targetAccount, amount);
-}
-
-void task3()
+int main()
 {
 	BankAccount firstAccount;
 	firstAccount.balance = 10000;
@@ -49,6 +24,7 @@ void task3()
 	firstAccount.owner = "Jan Kowalski";
 
 	firstAccount.AccountInformation();
+	//accountInformation(firstAccount);
 
 	BankAccount secondAccount;
 	secondAccount.balance = 15000;
@@ -57,19 +33,13 @@ void task3()
 
 	secondAccount.AccountInformation();
 
-	depositToAccount(firstAccount, -14.50);
+	firstAccount.DepositToAccount(-14.50);
 	firstAccount.AccountInformation();
 
-	widthdrawalFromAccount(firstAccount, 14.50);
+	firstAccount.WidthdrawalFromAccount(14.50);
 	firstAccount.AccountInformation();
 
-	transferBetweenAcounts(secondAccount, firstAccount, 1000);
+	secondAccount.TransferBetweenAcounts(firstAccount, 1000);
 	firstAccount.AccountInformation();
 	secondAccount.AccountInformation();
 }
-
-int main()
-{
-	
-}
-
