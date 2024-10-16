e659823e74573c3c698c4e7511b3b9b66318f327
q <q>
2024-10-16 08:51:44 +0200

    xcvvb

--- Programownia Obiektowe/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ Programownia Obiektowe/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -14,18 +14,37 @@ void task1()
 	cin >> age;
 
 	ofstream writeInFile;
-	writeInFile.open("c:\\dane.txt");
+	writeInFile.open("c:\\dane.txt", ios::app);
 
 	if (writeInFile.is_open() == true)
 	{
 		writeInFile << name;
 		writeInFile << "\n";
+		writeInFile.flush(); 
 		writeInFile << age;
+		writeInFile << "\n";
 
 		writeInFile.close();
 	}
 	else
 		cout << "B³¹d otwarcia pliku\n";
+}
+
+void task2()
+{
+	string name;
+	int age;
+	ifstream readFromFile;
+	readFromFile.open("c:\\dane.txt");
+	if (readFromFile.is_open())
+	{
+		readFromFile >> name;
+		readFromFile >> age;
+		cout << "Imiê: " << name << " wiek: " << age << "\n";
+
+		readFromFile.close();
+	}
+
 }
 
 int main()
