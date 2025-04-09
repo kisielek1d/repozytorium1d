644b17947929be83c7eaa1c75ffbcdce2833347d
q <q>
2025-04-09 08:20:34 +0200

    asd

--- Matura2/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
+++ Matura2/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
@@ -1,123 +1,81 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    ifstream file("przyklad.txt");
-    vector<int> numbers;
+int odwrotnaLiczba(int num) {
+    int odwrtonyNum = 0;
+    while (num != 0) {
+        odwrtonyNum = odwrtonyNum * 10 + (num % 10);
+        num /= 10;
+    }
+    return odwrtonyNum;
+}
 
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+vector<int> odczytajLiczbyZPliku(const string& filename) {
+    ifstream file(filename);
+    vector<int> liczby;
     int n;
-    while (file >> n)
-    {
-        numbers.push_back(n);
+    while (file >> n) {
+        liczby.push_back(n);
     }
-    
-    cout << "zad 4.1:\n";
-    for (int num : numbers)
-    {
-        int reversedNum = 0;
-        int tempNum = num;
+    return liczby;
+}
 
-        do
-        {
-            reversedNum = reversedNum * 10;
-            reversedNum = reversedNum + (tempNum % 10);
-            tempNum = tempNum / 10;
-        } while (tempNum != 0);
-        if (reversedNum % 17 == 0)
-        {
-            cout << reversedNum << ", ";
+int main() {
+    vector<int> liczby = odczytajLiczbyZPliku("liczby.txt");
+
+    ofstream wynik("wyniki4.txt");
+
+    wynik << "Zad 4.1:";
+    for (int num : liczby) {
+        int odwrtona = odwrotnaLiczba(num);
+        if (odwrtona % 17 == 0) {
+            wynik << " " << odwrtona << ",";
         }
     }
-    cout << "\nzad 4.2:\n";
-    int max = 0;
-    int maxNum = 0;
-    for (int num : numbers)
-    {
-        int reversedNum = 0;
-        int tempNum = num;
-        int n = 0;
 
-        do
-        {
-            reversedNum = reversedNum * 10;
-            reversedNum = reversedNum + (tempNum % 10);
-            tempNum = tempNum / 10;
-        } while (tempNum != 0);
-        tempNum = num;
-
-        n = num - reversedNum;
-        if (n < 0)
-        {
-            n = n * -1;
-        }
-        if (n > max)
-        {
-            max = n;
+    int maxDiff = 0, maxNum = 0;
+    wynik << "\nZad 4.2:";
+    for (int num : liczby) {
+        int odwrtona = odwrotnaLiczba(num);
+        int diff = abs(num - odwrtona);
+        if (diff > maxDiff) {
+            maxDiff = diff;
             maxNum = num;
         }
     }
-    cout << maxNum << ", " << max;
-    cout << "\nzad 4.3:\n";
+    wynik << " " << maxNum << " " << maxDiff;
 
-    for (int num : numbers)
-    {
-        int reversedNum = 0;
-        int tempNum = num;
+    wynik << "\nZad 4.3:";
+    for (int num : liczby) {
+        int odwrtona = odwrotnaLiczba(num);
+        if (isPrime(num) && isPrime(odwrtona)) {
+            wynik << " " << num << ",";
+        }
+    }
 
-        do
-        {
-            reversedNum = reversedNum * 10;
-            reversedNum = reversedNum + (tempNum % 10);
-            tempNum = tempNum / 10;
-        } while (tempNum != 0);
+    map<int, int> counter;
+    for (int num : liczby) {
+        counter[num]++;
+    }
+    int countDifferent = counter.size(), countTwo = 0, countThree = 0;
+    for (auto& entry : counter) {
+        if (entry.second == 2) countTwo++;
+        if (entry.second == 3) countThree++;
+    }
 
-        bool isPrime = true;
+    wynik << "\nZad 4.4: " << countDifferent << " " << countTwo << " " << countThree;
 
-        for (int i = 2; i < num; i++)
-        {
-            if (num % i == 0 || reversedNum % i == 0)
-            {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime == true)
-        {
-            cout << num << ", ";
-        }
-    }
-    int counter[10000];
-    for (int i = 0; i < 10000; i++)
-    {
-        counter[i] = 0;
-    }
-    cout << "\nzad 4.4:\n";
-    for (int num : numbers)
-    {
-        for (int i = 0; i < 10000; i++)
-        {
-            if (i == num)
-            {
-                counter[i]++;
-            }
-        }
-    }
-    for (int i = 0; i < 10000; i++)
-    {
-        if (counter[i] != 0)
-        {
-            cout << i << " ,";
-        }
-    }
-    cout << "\n";
-    for (int i = 0; i < 10000; i++)
-    {
-        if (counter[i] == 2)
-        {
-            cout << i << " ,";
-        }
-    }
+    return 0;
 }
