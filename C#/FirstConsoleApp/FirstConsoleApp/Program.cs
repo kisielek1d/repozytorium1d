b6883c364ba886f314daf4b964d0f52479d4d8a4
Sala213 <Sala213@ezse.pl>
2025-09-09 15:03:16 +0200

    dfg

--- C#/FirstConsoleApp/FirstConsoleApp/Program.cs
+++ C#/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -10,3 +10,21 @@ Console.WriteLine($"Witaj {surname} {name} tutaj!!!!");
 
 Console.WriteLine("Prędkość to km\\h");
 Console.WriteLine(@"Prędkość to km\h");
+
+int firstNumber = 5;
+int secondNumber = firstNumber;
+secondNumber = 6;
+Console.WriteLine($"firstNumber = {firstNumber}");
+Console.WriteLine($"secondNumber = {secondNumber}");
+
+//---------------------------------------------------------------------------------
+
+void ParameterTest_v1()
+{
+    Console.WriteLine($"Przed zmianą ParameterTest_v1 {firstNumber}");
+}
+
+firstNumber = 99;
+Console.WriteLine($"Przed ParameterTest_v1 {firstNumber}");
+ParameterTest_v1(firstNumber);
+Console.WriteLine($"Przed ParameterTest_v1 {firstNumber}");
