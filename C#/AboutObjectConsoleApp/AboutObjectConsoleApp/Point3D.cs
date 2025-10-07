38ec3dddfc7c6070621208a0561bbe11ebfb5dc8
Sala213 <Sala213@ezse.pl>
2025-10-07 14:47:40 +0200

    dfg

---
+++ C#/AboutObjectConsoleApp/AboutObjectConsoleApp/Point3D.cs
@@ -0,0 +1,41 @@
+﻿
+namespace AboutObjectConsoleApp;
+
+internal class Point3D : Point
+{
+    protected int z;
+
+    public Point3D() /*: base()*/
+    {
+        z = 64;
+    }
+
+    public Point3D(int q, int w, int e) : base(q, w)
+    {
+        //x = q;
+        //Y = w;
+        z = e;
+    }
+
+    public new void Show()
+    {
+        Console.WriteLine($"({x},{Y},{z})");
+        base.Show();
+    }
+}
+
+
+class A /*: C*/
+{
+
+}
+
+class B : A
+{
+
+}
+
+class C : B
+{
+
+}
