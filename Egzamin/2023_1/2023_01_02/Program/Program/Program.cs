1f19254f71f6caa5222cd66c3567d819eb4235b6
Sala213 <Sala213@ezse.pl>
2025-11-05 12:45:37 +0100

    sfd

---
+++ Egzamin/2023_1/2023_01_02/Program/Program/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/********************************************
+klasa:     Notatka
+opis:      Klasa reprezentuje pojedynczą notatkę zawierającą tytuł i treść.
+pola:      licznikNotatek – przechowuje liczbę utworzonych notatek (pole statyczne)
+           id – identyfikator notatki (nadawany automatycznie)
+           tytul – przechowuje tytuł notatki
+           tresc – przechowuje treść notatki
+autor:     Filip Ciesielski
+********************************************/
+
+
+namespace NotatkiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Aplikacja do obsługi notatek ===\n");
+
+            Notatka notatka1 = new Notatka("Zakupy", "Kupić mleko, chleb i ser.");
+            Notatka notatka2 = new Notatka("Spotkanie", "Spotkanie o 14:00 w sali konferencyjnej.");
+
+            Console.WriteLine("\n--- Wyświetlanie notatek ---");
+            notatka1.WyswietlNotatke();
+            Console.WriteLine();
+            notatka2.WyswietlNotatke();
+
+            Console.WriteLine("\n--- Diagnostyka ---");
+            notatka1.Diagnostyka();
+            notatka2.Diagnostyka();
+        }
+    }
+}
---
+++ Egzamin/2023_1/2023_01_02/Program/Program/Task4.cs
@@ -0,0 +1,29 @@
+﻿namespace NotatkiApp
+{
+    class Notatka
+    {
+        private static int licznikNotatek = 0;
+        private int id;
+        protected string tytul;
+        protected string tresc;
+
+        public Notatka(string tytul, string tresc)
+        {
+            licznikNotatek++;
+            id = licznikNotatek;
+            this.tytul = tytul;
+            this.tresc = tresc;
+        }
+
+        public void WyswietlNotatke()
+        {
+            System.Console.WriteLine($"Tytuł: {tytul}");
+            System.Console.WriteLine($"Treść: {tresc}");
+        }
+
+        public void Diagnostyka()
+        {
+            System.Console.WriteLine($"ID: {id}; Licznik: {licznikNotatek}; Tytuł: {tytul}; Treść: {tresc}");
+        }
+    }
+}
